adde295162427da9fbcf9b55604117730f5e6570
EC2 Default User <ec2-user@ip-172-31-31-58.ap-northeast-1.compute.internal>
2020-11-15 04:46:41 +0000

    [Update]index,show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,28 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
+    @user = current_user
+    @tweet = Tweet.new
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @tweet = Tweet.new
+    @tweets = @user.tweets
   end
 
   def edit
   end
+  
+  def update
+  end
 
   def out
   end
+  
+  def quit
+  end
+  
 end
