c8e2b6d178f283bc2b8e512ea448803dc8bb6c6c
EC2 Default User <ec2-user@ip-172-31-31-58.ap-northeast-1.compute.internal>
2020-11-16 07:05:51 +0000

    [Fix]comment_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_050043) do
+ActiveRecord::Schema.define(version: 2020_11_16_070244) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tweet_id"
-    t.text "comment"
+    t.text "tweet_comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
