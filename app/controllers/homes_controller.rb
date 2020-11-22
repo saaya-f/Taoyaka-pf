13f1d716e9b7e8037c1de69f2e1d2ee562b09d44
EC2 Default User <ec2-user@ip-172-31-31-58.ap-northeast-1.compute.internal>
2020-11-22 08:22:41 +0000

    [Fix]top,datetime

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,10 @@
 class HomesController < ApplicationController
 
   def top
+    @tweets = Tweet.page(params[:page]).per(4).order(created_at: :desc)
+  end
+  
+  def signed_in?
+    
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Taoyaka
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    # デフォルト言語を日本語に設定
+    config.i18n.default_locale = :ja
   end
 end
