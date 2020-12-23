ceb326e8d70a19a3af1cd6b8b8cf1f02b72b1d10
EC2 Default User <ec2-user@ip-172-31-31-58.ap-northeast-1.compute.internal>
2020-12-23 12:53:52 +0000

    [Add]follow_ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,18 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(@user)
-    redirect_to user_path(@user)
+    respond_to do |format|
+      format.html { redirect_to @user}
+      format.js
+    end
   end
 
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(@user)
-    redirect_to user_path(@user)
+    respond_to do |format|
+      format.html { redirect_to @user}
+        format.js
+    end
   end
 end
